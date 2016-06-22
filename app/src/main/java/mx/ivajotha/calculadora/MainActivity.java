658fd44951e5d0f623bfd33893d5d1cb2c2ab4db
ivajotha <jonathan@MacBook-Pro-de-Ivan.local>
2016-06-22 17:24:35 -0500

    Botton Click working

--- app/src/main/java/mx/ivajotha/calculadora/MainActivity.java
+++ app/src/main/java/mx/ivajotha/calculadora/MainActivity.java
@@ -2,12 +2,29 @@ package mx.ivajotha.calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.getBoxBinari).setOnClickListener(this);
+        findViewById(R.id.getBoxDecimal).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.getBoxBinari:
+                Toast.makeText(getApplicationContext(),"Binario",Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.getBoxDecimal:
+                Toast.makeText(getApplicationContext(),"Decimal",Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
