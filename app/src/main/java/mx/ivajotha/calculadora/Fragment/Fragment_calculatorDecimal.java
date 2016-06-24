29d123c377b5468cebecf2caed50301693e3cf73
ivajotha <jonathan@MacBook-Pro-de-Ivan.local>
2016-06-24 00:17:51 -0500

    calculator Decimal layout working

--- app/src/main/java/mx/ivajotha/calculadora/Fragment/Fragment_calculatorDecimal.java
+++ app/src/main/java/mx/ivajotha/calculadora/Fragment/Fragment_calculatorDecimal.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import mx.ivajotha.calculadora.R;
 
@@ -26,12 +30,10 @@ public class Fragment_calculatorDecimal extends Fragment {
         return f;
     }
 
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.calculator_decimal, container, false);
-
         return v;
     }
 }
